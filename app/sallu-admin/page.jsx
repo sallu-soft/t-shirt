459c316a429637f646928fb134c3053d96400363
Dev-Shakil <shakilhossain1007@gmail.com>
2024-05-11 15:43:19 +0600

    Picture ed

---
+++ app/sallu-admin/page.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import OrderList from '../components/OrderList';
+
+const getOrders = async ()=>{
+    try{
+        const res = await fetch(`${process.env.NEXT_PUBLIC_CORS}/api/orders`,{
+            
+            revalidate: 10,
+        })
+        if(!res.ok){
+            throw new Error("Failed to fetch Orders");
+           
+        }
+        return res.json();
+    }catch(error){
+        console.log("Error While fetching Orders:" + error)
+    }
+}
+const page = async () => {
+    const {orders} = await getOrders();
+   return (
+    <div>
+        <OrderList Orders={orders}/>
+        
+    </div>
+  )
+}
+
+export default page
+
+
+
