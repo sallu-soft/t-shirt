1cc67f92a44d530d76f8b9f2e5f2cf4b212b8684
Dev-Shakil <shakilhossain1007@gmail.com>
2024-05-11 15:53:42 +0600

    helll

--- app/sallu-admin/page.jsx
+++ app/sallu-admin/page.jsx
@@ -5,7 +5,7 @@ const getOrders = async ()=>{
     try{
         const res = await fetch(`${process.env.NEXT_PUBLIC_CORS}/api/orders`,{
             
-            revalidate: 10,
+            cache:"no-store"
         })
         if(!res.ok){
             throw new Error("Failed to fetch Orders");
@@ -30,3 +30,37 @@ export default page
 
 
 
+// const getOrders = async () => {
+//     try {
+//       const res = await fetch(`${process.env.NEXT_PUBLIC_CORS}/api/orders`, {
+//         revalidate: 10,
+//       });
+//       if (!res.ok) {
+//         throw new Error("Failed to fetch Orders");
+//       }
+//       const data = await res.json();
+//       if (!data) {
+//         return { orders: [] }; // Return a default value if data is null or undefined
+//       }
+//       return data;
+//     } catch (error) {
+//       console.error("Error While fetching Orders:", error);
+//       return { error: true, message: "Failed to fetch orders" }; // Return an error object
+//     }
+//   };
+  
+//   const page = async () => {
+//     const response = await getOrders();
+//     if (response.error) {
+//       return <div>Error: {response.message}</div>;
+//     }
+  
+//     const orders = response.orders || []; // Use a default value if orders is null or undefined
+//     return (
+//       <div>
+//         <OrderList orders={orders} />
+//       </div>
+//     );
+//   };
+  
+//   export default page;
