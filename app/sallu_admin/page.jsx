3d36a68d0b9a9123e5fef78d7fecf2f3385c7eac
Dev-Shakil <shakilhossain1007@gmail.com>
2024-05-11 17:50:02 +0600

    added sallu_admin

---
+++ app/sallu_admin/page.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import OrderList from '../components/OrderList';
+
+const getOrders = async ()=>{
+    try{
+        const res = await fetch(`api/orders`,{
+            cache:"no-store",
+            revalidate: 10,
+        })
+        if(!res.ok){
+            throw new Error("Failed to fetch Orders");
+           
+        }
+        return res.json();
+    }catch(error){
+        console.log("Error While fetching Orders:" + error)
+    }
+}
+const page = async () => {
+    const {orders} = await getOrders();
+   return (
+    <div>
+        <OrderList Orders={orders}/>
+        
+    </div>
+  )
+}
+
+export default page
+
+
+
