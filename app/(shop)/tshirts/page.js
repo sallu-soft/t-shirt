3479f42af98c1b6000d38e27205000e4588fddfb
sallu-soft <mdsalauddinm332@gmail.com>
2024-11-11 17:35:12 +0600

    ehl

--- app/(shop)/tshirts/page.js
+++ app/(shop)/tshirts/page.js
@@ -1,24 +1,11 @@
-import { Button } from '@/components/ui/button'
+
 import React from 'react'
-import ReactImageMagnify from 'react-image-magnify'
 
 const Tshirts = () => {
   
   return (
     <div className="fluid__image-container">
-    <ReactImageMagnify {...{
-        smallImage: {
-            alt: 'Wristwatch by Ted Baker London',
-            isFluidWidth: true,
-            src: watchImg687,
-        },
-        largeImage: {
-            src: watchImg1200,
-            width: 1200,
-            height: 1800
-        },
-        shouldUsePositiveSpaceLens: true
-    }} />
+   
 </div>
   )
 }
